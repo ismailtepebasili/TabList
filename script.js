c9b769b7a97eb01956c8cc0c9e0a95d801aafd85
İsmail TEPEBAŞILI <137713349+ismailtepebasili@users.noreply.github.com>
2025-05-26 16:31:36 +0300

    Added button color change feature

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ function openTabLondon() {
    document.getElementById("tab1-content").classList.remove("tabInactive");
    document.getElementById("tab2-content").classList.add("tabInactive");
    document.getElementById("tab3-content").classList.add("tabInactive");
+   document.getElementById("tab1-button").classList.add("btnColor");
+   document.getElementById("tab2-button").classList.remove("btnColor");
+   document.getElementById("tab3-button").classList.remove("btnColor");
 }
 
 function openTabParis() {
     document.getElementById("tab2-content").classList.remove("tabInactive");
     document.getElementById("tab1-content").classList.add("tabInactive");
     document.getElementById("tab3-content").classList.add("tabInactive");
+    document.getElementById("tab2-button").classList.add("btnColor");
+    document.getElementById("tab1-button").classList.remove("btnColor");
+    document.getElementById("tab3-button").classList.remove("btnColor");
 }
 
 function openTabTokyo() {
     document.getElementById("tab3-content").classList.remove("tabInactive");
     document.getElementById("tab1-content").classList.add("tabInactive");
     document.getElementById("tab2-content").classList.add("tabInactive");
+    document.getElementById("tab3-button").classList.add("btnColor");
+    document.getElementById("tab1-button").classList.remove("btnColor");
+    document.getElementById("tab2-button").classList.remove("btnColor");
 }
+
 
 
